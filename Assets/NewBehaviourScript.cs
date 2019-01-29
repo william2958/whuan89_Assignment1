dd47b18c9d3a1e9cd869a68632937b7999277bba
William Huang <william2958@gmail.com>
2019-01-28 20:46:12 -0500

    Added reset shape functionality

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+  Vector3 ogPosition;
+  public Quaternion ogRotation;
+
+  // Called before first frame update
+  void Start()
+  {
+    ogPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+    ogRotation = transform.rotation;
+  }
+
+  // Called once per frame
+  void Update()
+  {
+
+  }
+
+  private void OnMouseOver()
+  {
+    // If it's the right mouse button
+    if (Input.GetMouseButtonDown(1))
     {
-        
+      // Reset the positions
+      gameObject.transform.position = ogPosition;
+      gameObject.transform.rotation = ogRotation;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 }
